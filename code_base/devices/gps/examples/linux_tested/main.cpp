47b088609a5ac2bc1471928002306c219698e948
Valery <tor-tech@mail.ru>
2026-02-08 21:24:02 +0300

    WIP (CLI)

--- code_base/devices/gps/examples/linux_tested/main.cpp
+++ code_base/devices/gps/examples/linux_tested/main.cpp
@@ -1,3 +1,4 @@
+#include <QCommandLineParser>
 #include <QCoreApplication>
 #include <QDebug>
 #include <QObject>
@@ -43,28 +44,66 @@ void watchForQuitKey() {
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("gps-cli");
+    QCoreApplication::setApplicationVersion("1.0");
 
     // Устанавливаем обработчики сигналов
     std::signal(SIGINT, handleSignal);
     std::signal(SIGTERM, handleSignal);
 
+    QCommandLineParser parser;
+    parser.setApplicationDescription("com gps cli module");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    // Опции
+    QCommandLineOption pathOption({"p", "path"}, "working directory", "path");
+    parser.addOption(pathOption);
+
+    QCommandLineOption comOption({"c", "com"}, "com port name", "/dev/pts/1");
+    parser.addOption(comOption);
+
+    QCommandLineOption baudRateOption({"b", "baud"}, "9600", "logfile");
+    parser.addOption(baudRateOption);
+
+    QCommandLineOption logOption({"l", "log"}, "path to gps.log.", "logfile");
+    parser.addOption(logOption);
+
+    QCommandLineOption formatOption(
+        {"f", "format"}, "Format for gps data (CSV or JSON).", "format");
+    parser.addOption(formatOption);
+
+    parser.process(app);
+
+    QString path = parser.value(pathOption);
+    QString comPort = parser.value(comOption);
+    QString logFile = parser.value(logOption);
+    QString format =
+        parser.value(formatOption).toUpper();  // нормализуем в верхний регистр
+
+    qDebug() << "Path:" << path;
+    qDebug() << "COM Port:" << comPort;
+    qDebug() << "Log File:" << logFile;
+    qDebug() << "Format:" << format;
+
+    // Проверка формата без учёта регистра
+    if (format == "CSV") {
+        qDebug() << "CSV format";
+    } else if (format == "JSON") {
+        qDebug() << "JSON format";
+    } else {
+        format = "CSV";
+        qDebug() << "CSV as default";
+    }
+
     gps_receiver = new GPSReceiver;
     QObject::connect(gps_receiver, &GPSParser::gpsDataUpdated,
                      [&](const GpsData data) {
-                         qDebug() << gps::toCsvString(data);
                          logger::saveGpsDataToLogFile(data);
+                         qDebug() << gps::toStyledString(data);
                      });
 
-    // Проверяем аргументы командной строки
-    QString portPath;
-    if (argc > 1) {
-        portPath = QString(argv[1]);
-    } else {
-        qWarning() << "default COM port will be used.....";
-        portPath = QString("/dev/pts/1");
-    }
-
-    future = QtConcurrent::run(gps_receiver, &GPSReceiver::start, portPath);
+    future = QtConcurrent::run(gps_receiver, &GPSReceiver::start, comPort);
 
     QObject::connect(&app, &QCoreApplication::aboutToQuit, &app, [&]() {
         gps_receiver->stop();
