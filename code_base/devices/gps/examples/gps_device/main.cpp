885946e4b4ad51353a972332b04cb8b6a86b0c05
ValeryStk <tor-tech@mail.ru>
2026-02-12 19:11:04 +0300

    WIP (CLI template)

--- code_base/devices/gps/examples/gps_device/main.cpp
+++ code_base/devices/gps/examples/gps_device/main.cpp
@@ -1,6 +1,15 @@
 #include <QApplication>
+#include <QCommandLineParser>
 
+#include "csignal"
 #include "gps_device.h"
+#include "iostream"
+
+enum class GPS_MODULE_VARIANTS {
+    GPS_MODULE_CLI,
+    GPS_MODULE_WITHOUT_GUI,
+    GPS_MODULE_WIDGET
+};
 
 void exmpl_without_widget() {
     // 1. создаем объект GPSDevice
@@ -44,18 +53,67 @@ void exmpl_with_widget() {
     QTimer::singleShot(7000, [gps]() { delete gps; });
 }
 
+void exmpl_CLI(QApplication &app) {
+    QCommandLineParser parser;
+    parser.setApplicationDescription("com gps cli module");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    // Опции
+    QCommandLineOption pathOption({"p", "path"}, "working directory", "path");
+    parser.addOption(pathOption);
+
+    QCommandLineOption comOption({"c", "com"}, "com port name", "/dev/pts/1");
+    parser.addOption(comOption);
+
+    QCommandLineOption baudRateOption({"b", "baud"}, "9600", "logfile");
+    parser.addOption(baudRateOption);
+
+    QCommandLineOption logOption({"l", "log"}, "path to gps.log.", "logfile");
+    parser.addOption(logOption);
+
+    QCommandLineOption formatOption(
+        {"f", "format"}, "Format for gps data (CSV or JSON).", "format");
+    parser.addOption(formatOption);
+
+    parser.process(app);
+
+    QString path = parser.value(pathOption);
+    QString comPort = parser.value(comOption);
+    QString logFile = parser.value(logOption);
+    QString format =
+        parser.value(formatOption).toUpper();  // нормализуем в верхний регистр
+
+    qDebug() << "Path:" << path;
+    qDebug() << "COM Port:" << comPort;
+    qDebug() << "Log File:" << logFile;
+    qDebug() << "Format:" << format;
+
+    // Проверка формата без учёта регистра
+    if (format == "CSV") {
+        qDebug() << "CSV format";
+    } else if (format == "JSON") {
+        qDebug() << "JSON format";
+    } else {
+        format = "CSV";
+        qDebug() << "CSV as default";
+    }
+}
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    int example_num = 1;  // выбери номер примера
+    GPS_MODULE_VARIANTS example_num = GPS_MODULE_VARIANTS::GPS_MODULE_WIDGET;
 
     switch (example_num) {
-        case 1:
-            exmpl_with_widget();
+        case GPS_MODULE_VARIANTS::GPS_MODULE_CLI:
+            exmpl_CLI(app);
             break;
-        case 2:
+        case GPS_MODULE_VARIANTS::GPS_MODULE_WITHOUT_GUI:
             exmpl_without_widget();
-        default:
+            break;
+        case GPS_MODULE_VARIANTS::GPS_MODULE_WIDGET:
+            exmpl_with_widget();
             break;
     }
 
