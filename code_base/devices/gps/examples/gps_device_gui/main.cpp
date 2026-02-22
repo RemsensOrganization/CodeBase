690bd2c9614d5b356fefe7beb882faf8e8dd86c9
Valery <tor-tech@mail.ru>
2026-02-23 00:11:26 +0300

    Update main.cpp (#12)

--- code_base/devices/gps/examples/gps_device_gui/main.cpp
+++ code_base/devices/gps/examples/gps_device_gui/main.cpp
@@ -1,33 +1,12 @@
 #include <QApplication>
 #include <QCommandLineParser>
 
-#include "gps_device.h"
 #include "mainwindow.h"
-
-void exmpl_with_widget() {
-    qRegisterMetaType<GpsData>("GpsData");
-    GPSDevice *gps = new GPSDevice;
-    GpsWidget *widget = gps->createWidget();  // создали виджет и теперь можно
-                                              // использовать его в своем GUI
-    widget->show();
-    gps->start();
-    gps->writeOriginGpsDataToFile();
-    gps->writeFormattedGpsDataToFile(logger::saveFormat::jsonIndented);
-
-    widget->setAttribute(Qt::WA_DeleteOnClose,
-                         true);  // Авто delete после close
-
-    // QObject::connect(widget, &QObject::destroyed, [&]() { gps->stop(); });
-    QTimer::singleShot(7000, [gps]() { gps->stop(); });
-}
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow *window = new MainWindow;
-    window->setAttribute(Qt::WA_DeleteOnClose,
-                         true);  // Авто delete после close
-
+    window->setAttribute(Qt::WA_DeleteOnClose, true);
     window->show();
-
     return app.exec();
 }
