b9753ae3a131e33a093b65c61e1b0741fe2fab8d
AntonMrt <antonmartenov@gmail.com>
2026-02-25 14:13:08 +0300

    update

--- code_base/devices/gps/examples/gps_device_gui/mainwindow.cpp
+++ code_base/devices/gps/examples/gps_device_gui/mainwindow.cpp
@@ -11,27 +11,28 @@ MainWindow::MainWindow(QWidget *parent)
     ui->statusbar->addPermanentWidget(pushButton_start);
     ui->statusbar->addPermanentWidget(pushButton_stop);
     m_gps_dock = new QDockWidget("GpsWidget", this);
-
     m_gps_dock->setAllowedAreas(Qt::AllDockWidgetAreas);
-    m_gps = new GPSDevice;
-    auto *w1 =
-        new GpsSimpleWidget(m_gps_dock);  // создали виджет и теперь можно
-                                          // использовать его в своем GUI
-    w1->setObjectName("GpsWidget");
-    m_gps->attachView(w1);
     m_gps_dock->setMaximumSize(QSize(200, 300));
 
-    m_gps_dock->setWidget(w1);
+    m_gps = new GPSDevice;
+    // создаем виджет для GPS и теперь можно использовать его в своем GUI.
+    // в данном примере виджет дополнительно помещен в QDockWidget
+    auto *w1 = new GpsSimpleWidget(m_gps_dock);
+    m_gps->attachView(w1);
+    m_gps->start();
+    m_gps->writeOriginGpsDataToFile();
+    m_gps->writeFormattedGpsDataToFile(logger::saveFormat::jsonIndented);
+
+    // настройки доквиджета и палитры виджета
     QPalette pal = w1->palette();
     pal.setColor(QPalette::Window, QColor("#101418"));
     pal.setColor(QPalette::WindowText, QColor("#4FFFE1"));
     w1->setPalette(pal);
     w1->setAutoFillBackground(true);
+    m_gps_dock->setWidget(w1);
     addDockWidget(Qt::LeftDockWidgetArea, m_gps_dock);
 
-    m_gps->start();
-    m_gps->writeOriginGpsDataToFile();
-    m_gps->writeFormattedGpsDataToFile(logger::saveFormat::jsonIndented);
+    // коннекты, в которых остановка и запуск GPS
     connect(pushButton_start, &QPushButton::clicked, this,
             &MainWindow::pushButton_start_clicked);
     connect(pushButton_stop, &QPushButton::clicked, this,
